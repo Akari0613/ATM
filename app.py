fbf4125489f792e17d5b2f36c989f2a60046ebb0
Akari0613 <riria0613mari@gmail.com>
2024-10-24 12:53:01 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Welcome to Flask World!'
+
+@app.route('/hello')
+def hello_wprld():
+    return 'Welcome to Flask World!!!!!'
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='localhost')
